5ef14d3be2144abff2d4f86894b81b32a0edf94b
psychobolt <560721+psychobolt@users.noreply.github.com>
2024-03-04 07:53:50 -0800

    increase delay

--- stories/Examples/UserPage/UserPage.stories.js
+++ stories/Examples/UserPage/UserPage.stories.js
@@ -6,7 +6,7 @@ const meta = {
   title: 'Examples/User Page',
   render: UserPage,
   parameters: {
-    chromatic: { delay: 20 },
+    chromatic: { delay: 3500 },
     // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'fullscreen',
     docs: {
